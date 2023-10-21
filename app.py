4f9c701fc750c5125b6d9bfb9ec5d6088902c27a
macbookpro <muhlis.asri@gmail.com>
2023-10-21 16:31:13 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import pandas as pd
+
+
+app = Flask(__name__)
+stations = pd.read_csv("data-small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+@app.route("/")
+def home():
+    return render_template("home.html", data=stations.to_html())
+
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
+    return {
+        "station" : station,
+        "date" : date,
+        "temperature" : temperature,
+    }
+
+@app.route("api/v1/<station>")
+def bystation(station):
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
